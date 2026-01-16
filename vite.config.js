f059521a7a1ea44ce492d3cd2df27ca96cddc728
Reshma <reshma4sheys2@gmail.com>
2026-01-16 11:49:27 +0530

    fix:updated vite config for both github and netlify

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/ASGMT-10/'
+// (this was already a comment) https://vite.dev/config/
+// export default defineConfig({
+  // plugins: [react()],
+  // base: '/ASGMT-10/'
+export default defineConfig(({ command, mode }) => {
+  return {
+    plugins: [react()],
+    // If we are building for GitHub Pages, use the repo name. 
+    // Otherwise (like on Netlify), use the root.
+    base: process.env.NODE_ENV === 'production' ? '/ASGMT-10/' : '/',
+  }
+
 })
